608128007e5d4032a6e935284902e1f889b36e01
Denis Bernard <denis@nyli.com>
2022-02-28 13:12:06 -0500

    letter scorign

--- letterlist.js
+++ letterlist.js
@@ -98,3 +98,85 @@ const letterArray =[
 "q",
 "z"
 ]
+
+const letterscore = [
+  {
+    "letter": "a",
+    "value": 1
+  },
+  {
+    "letter": "b",
+    "value": 3},
+  {
+    "letter": "c",
+    "value": 3},
+  {
+    "letter": "d",
+    "value": 2},
+  {
+    "letter": "e",
+    "value": 1},
+  {
+    "letter": "f",
+    "value": 4},
+  {
+    "letter": "g",
+    "value": 2},
+  {
+    "letter": "h",
+    "value": 4},
+  {
+    "letter": "i",
+    "value": 1},
+  {
+    "letter": "j",
+    "value": 8},
+  {
+    "letter": "k",
+    "value": 5},
+  {
+    "letter": "l",
+    "value": 1},
+  {
+    "letter": "m",
+    "value": 3},
+  {
+    "letter": "n",
+    "value": 1},
+  {
+    "letter": "o",
+    "value": 1},
+  {
+    "letter": "p",
+    "value": 3},
+  {
+    "letter": "q",
+    "value": 10},
+  {
+    "letter": "r",
+    "value": 1},
+  {
+    "letter": "s",
+    "value": 1},
+  {
+    "letter": "t",
+    "value": 1},
+  {
+    "letter": "u",
+    "value": 1},
+  {
+    "letter": "v",
+    "value": 4},
+  {
+    "letter": "w",
+    "value": 4},
+  {
+    "letter": "x",
+    "value": 8},
+  {
+    "letter": "y",
+    "value": 4},
+  {
+    "letter": "z",
+    "value": 10},
+]
